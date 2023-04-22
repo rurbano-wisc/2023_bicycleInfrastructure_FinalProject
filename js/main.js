9f909853687304403d65257ea9bbecd8a70c66d6
Gerald Heston <gerald@hestongis.com>
2023-04-22 12:29:15 -0700

    loading geojson layers
    just to get them loaded into the Leaflet map

--- js/main.js
+++ js/main.js
@@ -26,6 +26,8 @@
     var osm150ftNev = L.geoJSON();
     var osmCal = L.geoJSON();
     var osmNev = L.geoJSON();
+    var accidentsCal = L.geoJSON();
+    var accidentsNev = L.geoJSON();
 
     // function to initiate Leaflet map
     function createMap() {
@@ -45,7 +47,7 @@
         controlLayers.addTo(map);
 
         // // call getData function to process the point layer
-        // getData();
+        getData();
 
         // add a base layer control to the map - has to be individually with the .addBaseLayer() method, not as a group object
         controlLayers.addBaseLayer(bikeBasemap, "Bicycle base map");
@@ -65,7 +67,7 @@
     }; // end createMap()
 
 
-    //function to import the metro area boundary data geojson, style it, and add it to the layer control
+    //function to import the metro area boundary data and OSM geojson, style it, and add it to the layer control
     function getMetroAreaBoundaryData() {
         // style for metro Area boundaries
         var metroAreaBoundaryStyle = {
@@ -78,7 +80,7 @@
 
         var osm150ftStyle = {
             //fillColor: "#A65E44",
-            color: "#453dcf", //random color - fix it later
+            color: "red", //random color - fix it later
             weight: 1,
             opacity: 1,
             fillOpacity: 0
@@ -86,11 +88,20 @@
 
         var osmStyle = {
             //fillColor: "#A65E44",
-            color: "#123acf", //random color - fix it later
+            color: "orange", //random color - fix it later
             weight: 1,
             opacity: 1,
             fillOpacity: 0
         };
+
+        // var accidentStyle = {
+        //     radius: 8,
+        //     fillColor: "#ff7800",
+        //     color: "#000",
+        //     weight: 1,
+        //     opacity: 1,
+        //     fillOpacity: 0.8
+        // };
 
         // load the Nevada Metro Area boundary data
         fetch("data/CityMetros_Nev_geog.geojson")
@@ -149,7 +160,7 @@
                 return response.json();
             })
             .then(function (json) {
-                osmNev = L.geoJSON(json, osm150ftStyle)//.addTo(map); took this out so that it wouldn't be added to the map when it first loads, then it will turn on when the zoom reaches level 6
+                osmNev = L.geoJSON(json, osmStyle)//.addTo(map); took this out so that it wouldn't be added to the map when it first loads, then it will turn on when the zoom reaches level 6
 
                 // add the layer to the Layers control
                 controlLayers.addOverlay(osmNev, 'OSM NV');
@@ -162,14 +173,143 @@
                 return response.json();
             })
             .then(function (json) {
-                osmCal = L.geoJSON(json, osm150ftStyle)//.addTo(map); took this out so that it wouldn't be added to the map when it first loads, then it will turn on when the zoom reaches level 6
+                osmCal = L.geoJSON(json, osmStyle)//.addTo(map); took this out so that it wouldn't be added to the map when it first loads, then it will turn on when the zoom reaches level 6
 
                 // add the layer to the Layers control
                 controlLayers.addOverlay(osmCal, 'OSM CA');
             });
+
+
     }; // end getMetroAreaBoundaryData()
 
     document.addEventListener('DOMContentLoaded', createMap);
 
+// function to retrieve the data and place it on the map
+function getData() {
+
+    // load the Nevada accidents data
+    fetch("data/Accidents_Nevada.geojson")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (json) {
+            //create an attribute array - names of attributes
+            var attributes = processData(json);
+            // calculate the minimum value
+ //           calcStats(json);
+
+            // calculate the yearly stats
+ //           calcYearlyStats(json);
+            //call function to create proportional symbols
+            createPropSymbols(json, attributes)
+            //controlLayers.addOverlay(json, 'Accidents NV');
+            //call function to create the proportional symbols
+//            createSequenceControls(attributes);
+            // call function to create the legend with text for the 1st year, 2016
+//            createLegend(attributes[0]);
+        });
+
+
+
+            // load the California accidents data
+    fetch("data/Accidents_California.geojson")
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (json) {
+        //create an attribute array - names of attributes
+        var attributes = processData(json);
+        // calculate the minimum value
+//           calcStats(json);
+
+        // calculate the yearly stats
+//           calcYearlyStats(json);
+        //call function to create proportional symbols
+        createPropSymbols(json, attributes)
+        //controlLayers.addOverlay(json, 'Accidents NV');
+        //call function to create the proportional symbols
+//            createSequenceControls(attributes);
+        // call function to create the legend with text for the 1st year, 2016
+//            createLegend(attributes[0]);
+    });
+}; // end getData();
+
+//build an attributes array from the data
+function processData(data) {
+    //empty array to hold attributes
+    var attributes = [];
+
+    //properties of the 1st feature in the dataset
+    var properties = data.features[0].properties;
+
+    //push each attribute name into attributes array
+    for (var attribute in properties) {
+        //only take attributes with population values
+        //if (attribute.indexOf("Rate") > -1) {
+            attributes.push(attribute);
+        //};
+    };
+
+    return attributes;
+}; // end processData
+
+function createPropSymbols(data, attributes) {
+    // create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(data, {
+        pointToLayer: function (feature, latlng) {
+            return pointToLayer(feature, latlng, attributes);
+            
+        }
+    }).addTo(map);
+    //controlLayers.addOverlay(data, 'Accidents NV');
+    
+}; // end createPropSymbols
+
+//function to convert markers to circle markers
+function pointToLayer(feature, latlng, attributes) {
+    // mapping average unemployment rate for each year
+
+    var attribute = attributes[0];
+
+    // style for brown circle
+    var geoJsonMarkerOptions = {
+        radius: 6,
+        fillColor: "#A65E44",
+        color: "#fff",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+
+    //step 5, for each feature, determine its value for the selected attribute
+    //var attValue = Number(feature.properties[attribute]);
+
+    // step 6, give each feature's circle marker a radius based on its attribute value
+    //geoJsonMarkerOptions.radius = calcPropRadius(attValue);
+
+    //create circle marker layer
+    var layer = L.circleMarker(latlng, geoJsonMarkerOptions);
+
+    //build popup content string
+    var popupContent = new PopupContent(feature.properties, attribute);
+
+    //bind the popup to the circle marker
+    layer.bindPopup(popupContent.formatted, {
+        offset: new L.Point(0, -geoJsonMarkerOptions.radius)
+    });
+
+    //return the circle marker to the L.geoJson pointToLayer option
+    return layer;
+}; // end pointToLayer
+
+
+// function to create a PopupContent class
+function PopupContent(properties, attribute) {
+    this.properties = properties;
+    this.attribute = attribute;
+    //this.year = attribute.split("_")[1];
+    //this.unemploymentRate = this.properties[attribute];
+    this.formatted = "<p><b>Year: " + this.properties.YEAR + "</b></p><p>Persons " + this.properties.PERSONS + "</b><h4>Fatalities " + this.properties.FATALS + "</h4></p>";
+}; // end PopupContent
 
 })(); // end of wrapper function
