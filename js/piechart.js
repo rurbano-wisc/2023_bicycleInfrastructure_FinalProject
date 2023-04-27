03e2cb5a297ce3c1cdf673853cbe99badab95404
Gerald Heston <gerald@hestongis.com>
2023-04-27 14:07:41 -0700

    update piechart.js, formatting
    first chart is for all CA-NV

--- js/piechart.js
+++ js/piechart.js
@@ -1,56 +1,50 @@
-//tutorial on YouTube// https://youtu.be/L5GXOdt2uOc
+// Chart functions
+
 // wrapper function
 (function () {
 
   // frame size
   var width = 300, height = 350;
 
-  // ordinal color generator
-  //var colors = d3.scaleOrdinal(d3.schemeDark2);
-  //var colors = ['#fef0d9','#fdcc8a','#fc8d59','#e34a33','#b30000'];
-
+  // color generator, 2 colors for ped/bike
   var colorScale = d3.scaleOrdinal().domain(["Pedestrian", "Bicyclist"])
-  .range(["#d9a78b", "#a65e44"]);
+    .range(["#d9a78b", "#a65e44"]);
 
-  //pie.colors(function(d){ return colorScale(d.fruitType); });
-
-
-  // create an svg element
+  // create an svg element in the chart div
   var svg = d3.select(".chart")
     .append("svg")
     .attr("width", width)
     .attr("height", height)
-    //.style("background", "pink");
-
-
-var pieTranslate = "translate(150, 180)";
-var pieInnerRadius = 0;
-var pieOuterRadius = 100;
-
+  
+  // variables for the pie chart dimensions  
+  var pieTranslate = "translate(150, 180)";
+  var pieInnerRadius = 0;
+  var pieOuterRadius = 100;
 
   // hold the csv accident data
   var csvData;
 
   // expressed metro to start the app
-  var expressed = "Reno Metropolitan"
+  //var expressed = "Reno Metropolitan"
 
   window.onload = setMap();
 
-  // function to create a pie chart from 2 values from a Metro area
+  // function to create a pie chart, starting with all CA-NV Pedestrian & Bicyclists
   function setChart(metroPedBike) {
+    
     // pie generator - moves from details to data array, no sorting
     var data = d3.pie()
       .sort(null)
       .value(function (d) {
         return d.value;
       })(metroPedBike);
-    console.log(metroPedBike);
+    //console.log(metroPedBike);
 
     // arc generator - 
     var segments = d3.arc()
       .innerRadius(pieInnerRadius)
       .outerRadius(pieOuterRadius)
-      .padAngle(0.05)
+      //.padAngle(0.05)
       .padRadius(50);
 
     // appends the segments to the frame using data
@@ -65,13 +59,12 @@ var pieOuterRadius = 100;
       .attr("d", segments)
       .attr("class", "piesegment")
       .attr("fill", function (d) {
-        return colorScale(d.data.name); 
+        return colorScale(d.data.name);
       });
 
     // labels (number) at centroid of each segment (pie slice)
     var content = d3.select("g")
       .selectAll("text")
-      //.attr("class", "pielabels")
       .data(data);
 
     content.enter()
@@ -87,7 +80,7 @@ var pieOuterRadius = 100;
 
     // legend
     var legends = svg.append("g")
-    // makes this appear in upper left of chart
+      // makes this appear in upper left of chart
       .attr("transform", "translate(5, 0)")
       .selectAll(".legends")
       .data(data);
@@ -113,9 +106,6 @@ var pieOuterRadius = 100;
       .text(function (d) {
         return d.data.name;
       })
-      // .attr("fill", function(d) {
-      //   return colors(d.data.number)
-      // })
       .attr("x", 25)
       .attr("y", 15);
 
@@ -153,23 +143,21 @@ var pieOuterRadius = 100;
         var metroNameIndex = metroList.findIndex(item => item === d);
         return metroList[metroNameIndex];
       });
-
-    //setChart(sacramentoSumArray);
+   
 
   }; // end createDropdown()
 
-
+  // function to change the metro area displayed in the chart
   function changeAttribute(metroName, metroAccidents) {
 
+    // get the accidents from the selected metro area, then summarize on PERSONS, HARM_EV
     var metroSubset = metroAccidents.get(metroName);
     var metroSubsetSum = d3.rollup(metroSubset, v => d3.sum(v, d => d.PERSONS), d => d.HARM_EV)
-
     var metroSubsetSumArray = Array.from(metroSubsetSum, ([name, value]) => ({ name, value }));
-
     //console.log("metroSubsetSumArray", metroSubsetSumArray);
+    //console.log(metroName, metroSubset.length);
 
-    console.log(metroName, metroSubset.length);
-
+    // transition on pie chart - does this do anything?
     var sections = d3.selectAll(".piesegment")
       .transition()
       .delay(function (d, i) {
@@ -181,25 +169,26 @@ var pieOuterRadius = 100;
 
   } // end changeAttribute()
 
-
+  // function to update the current pie chart with the selected Metro area
   function updateChart(sections, metroPedBike) {
 
+    // pie generator
     var data = d3.pie()
       .sort(null)
       .value(function (d) {
         return d.value;
       })(metroPedBike);
-      
-      // remove the existing pie segments
-      var oldSections = d3.selectAll(".piechart");
-      oldSections.remove();
 
-      // now add the new segments
+    // remove the existing pie segments
+    var oldSections = d3.selectAll(".piechart");
+    oldSections.remove();
+
+    // now add the new segments
     // arc generator - 
     var segments = d3.arc()
       .innerRadius(pieInnerRadius)
       .outerRadius(pieOuterRadius)
-      .padAngle(0.05)
+      //.padAngle(0.05)
       .padRadius(50);
 
     // appends the segments to the frame using data
@@ -217,11 +206,11 @@ var pieOuterRadius = 100;
         return colorScale(d.data.name);
       });
 
-      // remove old labels
+    // remove old labels
     var oldInside = d3.selectAll(".inside")
     oldInside.remove();
 
-       // labels (number) at centroid of each segment (pie slice)
+    // labels (number) at centroid of each segment (pie slice)
     var content = d3.select(".piechart")
       .selectAll("text")
       .data(data);
@@ -242,41 +231,38 @@ var pieOuterRadius = 100;
   // read the csv data of accidents
   function setMap() {
 
+    // load the csv data
     var promises = [];
     promises.push(d3.csv("data/Accidents_Merge_metro2.csv", d3.autoType)); // d3.autoType reads in the input from d3.csv and tries to figure out the data type, converting numeric strings to numbers 
     Promise.all(promises).then(callback);
 
-
+    // callback function
     function callback(data) {
       csvData = data[0];
-      //console.log("Row 16999: ", csvData[16999]);
 
+      // groups all the accident records by their metro area
       var metroAccidents = d3.group(csvData, d => d.metro);
       //console.log(metroAccidents);
-
       //console.log(d3.flatRollup(csvData, v => d3.sum(v, d => d.PERSONS), d => d.metro, d => d.HARM_EV));
 
-      var metroList = Array.from(metroAccidents, ([key]) => (key));
+      var allMetroAccidentsSum = d3.rollup(csvData, v => d3.sum(v, d => d.PERSONS), d => d.HARM_EV);
+      //console.log("allMetroAccidentsSum: ", allMetroAccidentsSum);
+      var allMetroAccidentsSumArray = Array.from(allMetroAccidentsSum, ([name, value]) => ({ name, value }));
+      //console.log("allMetroAccidentsArray: ", allMetroAccidentsSumArray);
 
+      var metroList = Array.from(metroAccidents, ([key]) => (key));
       //console.log("MetroList: ", metroList);
 
-      var metroSubset = metroAccidents.get(expressed);
-      var metroSubsetSum = d3.rollup(metroSubset, v => d3.sum(v, d => d.PERSONS), d => d.HARM_EV)
+      //var metroSubset = metroAccidents.get(expressed);
+      //var metroSubsetSum = d3.rollup(metroSubset, v => d3.sum(v, d => d.PERSONS), d => d.HARM_EV)
+      //var metroSubsetSumArray = Array.from(metroSubsetSum, ([name, value]) => ({ name, value }));
+      //console.log("metroSubsetSumArray: ", metroSubsetSumArray);
 
-      var metroSubsetSumArray = Array.from(metroSubsetSum, ([name, value]) => ({ name, value }));
-
+      // call dropdown function
       createDropdown(metroAccidents, metroList)
 
-      // var sacramentoPed = sacramentoSumArray
-      //var sacramentoBike = sacramentoSumArray[1][1];
-
-      //console.log("metroSubsetSumArray: ", metroSubsetSumArray);
-      //console.log("Sacramento max persons: ", sacramentoPed, sacramentoBike);
-
-      //console.log("Sacramento row 0: ", sacramento[0]);
-      setChart(metroSubsetSumArray);
-
-
+      // create the 1st chart
+      setChart(allMetroAccidentsSumArray);
 
     }; // end callback()
 
