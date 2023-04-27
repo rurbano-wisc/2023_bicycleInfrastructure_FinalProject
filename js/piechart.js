88b78d34772b1d42e4cbdf7b8af7a2c5c2b4f5de
rurbano-wisc <120360519+rurbano-wisc@users.noreply.github.com>
2023-04-26 23:23:56 -0700

    Rachael 4/26 minor changes, added pie chart to bootstrap

---
+++ js/piechart.js
@@ -0,0 +1,246 @@
+//tutorial on YouTube// https://youtu.be/L5GXOdt2uOc
+// wrapper function
+(function () {
+
+  // frame size
+  var width = 460, height = 500;
+
+  // ordinal color generator
+  var colors = d3.scaleOrdinal(d3.schemeDark2);
+  //var colors = ['#fef0d9','#fdcc8a','#fc8d59','#e34a33','#b30000'];
+
+  // create an svg element, make it pink
+  var svg = d3.select(".chart")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .style("background", "pink");
+
+  // hold the csv accident data
+  var csvData;
+
+  // expressed metro to start the app
+  var expressed = "Reno Metropolitan"
+
+  window.onload = setMap();
+
+  // function to create a pie chart from 2 values from a Metro area
+  function setChart(metroPedBike) {
+    // pie generator - moves from details to data array, no sorting
+    var data = d3.pie()
+      .sort(null)
+      .value(function (d) {
+        return d.value;
+      })(metroPedBike);
+    //console.log(data);
+
+    // arc generator - 
+    var segments = d3.arc()
+      .innerRadius(0)
+      .outerRadius(200)
+      .padAngle(0.05)
+      .padRadius(50);
+
+    // appends the segments to the frame using data
+    var sections = svg.append("g")
+      .attr("transform", "translate(210, 250)")
+      .selectAll("path")
+      .data(data);
+
+    sections.enter()
+      .append("path")
+      .attr("d", segments)
+      .attr("class", "piesegment")
+      .attr("fill", function (d) {
+        return colors(d.data.value);
+      });
+
+    // labels (number) at centroid of each segment (pie slice)
+    var content = d3.select("g")
+      .selectAll("text")
+      .data(data);
+
+    content.enter()
+      .append("text")
+      .classed("inside", true)
+      .each(function (d) {
+        var center = segments.centroid(d);
+        d3.select(this)
+          .attr("x", center[0])
+          .attr("y", center[1])
+          .text(d.data.value)
+      })
+
+    // legend
+    var legends = svg.append("g")
+    // makes this appear in upper left of chart
+      .attr("transform", "translate(5, 0)")
+      .selectAll(".legends")
+      .data(data);
+
+    var legend = legends.enter()
+      .append("g")
+      .classed("legends", true)
+      .attr("transform", function (d, i) {
+        return "translate(0," + (i + 1) * 30 + ")";
+      });
+
+    // legend patches
+    legend.append("rect")
+      .attr("width", 20)
+      .attr("height", 20)
+      .attr("fill", function (d) {
+        return colors(d.data.value);
+      });
+
+    // legend labels
+    legend.append("text")
+      .classed("label", true)
+      .text(function (d) {
+        return d.data.name;
+      })
+      // .attr("fill", function(d) {
+      //   return colors(d.data.number)
+      // })
+      .attr("x", 30)
+      .attr("y", 18);
+
+  } // end setChart()
+
+  // function to create a dropdown menu for attribute selection
+  function createDropdown(metroAccidents, metroList) {
+    // add select element
+
+    //console.log(metroList);
+
+    var dropdown = d3.select(".chart")
+      .append("select")
+      .attr("class", "dropdown")
+      .on("change", function () {
+        changeAttribute(this.value, metroAccidents)
+
+      });
+
+    // add initial option
+    var titleOption = dropdown.append("option")
+      .attr("class", "titleOption")
+      .attr("disabled", "true")
+      .text("Select Attribute");
+
+    // add attribute name options
+    var attrOptions = dropdown.selectAll("attrOptions")
+      .data(metroList)
+      .enter()
+      .append("option")
+      .attr("value", function (d) {
+        return d;
+      })
+      .text(function (d) {
+        var metroNameIndex = metroList.findIndex(item => item === d);
+        return metroList[metroNameIndex];
+      });
+
+    //setChart(sacramentoSumArray);
+
+  }; // end createDropdown()
+
+
+  function changeAttribute(metroName, metroAccidents) {
+
+    var metroSubset = metroAccidents.get(metroName);
+    var metroSubsetSum = d3.rollup(metroSubset, v => d3.sum(v, d => d.PERSONS), d => d.HARM_EV)
+
+    var metroSubsetSumArray = Array.from(metroSubsetSum, ([name, value]) => ({ name, value }));
+
+    //console.log("metroSubsetSumArray", metroSubsetSumArray);
+
+    console.log(metroName, metroSubset.length);
+
+    var sections = d3.selectAll(".piesegment")
+      .transition()
+      .delay(function (d, i) {
+        return i * 20;
+      })
+      .duration(500);
+
+    updateChart(sections, metroSubsetSumArray)
+
+  } // end changeAttribute()
+
+
+  function updateChart(sections, metroPedBike) {
+
+    var data = d3.pie()
+      .sort(null)
+      .value(function (d) {
+        return d.value;
+      })(metroPedBike);
+
+      var oldsections = d3.selectAll(".piesegment");
+      oldsections.remove();
+
+    // arc generator - 
+    var segments = d3.arc()
+      .innerRadius(0)
+      .outerRadius(200)
+      .padAngle(0.05)
+      .padRadius(50);
+
+    // appends the segments to the frame using data
+    var sections = svg.append("g")
+      .attr("transform", "translate(250, 250)")
+      .selectAll("path")
+      .data(data);
+
+    sections.enter()
+      .append("path")
+      .attr("d", segments)
+      .attr("class", "piesegment")
+      .attr("fill", function (d) {
+        return colors(d.data.value);
+      });
+  }; // end updateChart()
+
+  // read the csv data of accidents
+  function setMap() {
+
+    var promises = [];
+    promises.push(d3.csv("data/Accidents_Merge_metro2.csv", d3.autoType)); // d3.autoType reads in the input from d3.csv and tries to figure out the data type, converting numeric strings to numbers 
+    Promise.all(promises).then(callback);
+
+
+    function callback(data) {
+      csvData = data[0];
+      //console.log("Row 16999: ", csvData[16999]);
+
+      var metroAccidents = d3.group(csvData, d => d.metro);
+      //console.log(metroAccidents);
+
+
+      var metroList = Array.from(metroAccidents, ([key]) => (key));
+
+      console.log("MetroList: ", metroList);
+
+      var metroSubset = metroAccidents.get("Reno Metropolitan");
+      var metroSubsetSum = d3.rollup(metroSubset, v => d3.sum(v, d => d.PERSONS), d => d.HARM_EV)
+
+      var metroSubsetSumArray = Array.from(metroSubsetSum, ([name, value]) => ({ name, value }));
+
+      createDropdown(metroAccidents, metroList)
+
+      // var sacramentoPed = sacramentoSumArray
+      //var sacramentoBike = sacramentoSumArray[1][1];
+
+      //console.log("metroSubsetSumArray: ", metroSubsetSumArray);
+      //console.log("Sacramento max persons: ", sacramentoPed, sacramentoBike);
+
+      //console.log("Sacramento row 0: ", sacramento[0]);
+      setChart(metroSubsetSumArray);
+
+
+
+    }; // end callback()
+
+  }; // end setMap()
+
+})(); // end of wrapper function
